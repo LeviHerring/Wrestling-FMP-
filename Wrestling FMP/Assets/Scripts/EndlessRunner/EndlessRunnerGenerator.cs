f2b1ebc4c5e8ba341038f0f10059e3226570a0a7
LeviHerring <514121@escg.ac.uk>
2022-04-20 15:58:28 +0100

    More art and tried to do endless runner

---
+++ Wrestling FMP/Assets/Scripts/EndlessRunner/EndlessRunnerGenerator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessRunnerGenerator : MonoBehaviour
+{
+    private const float playerDistanceSpawnLevel = 200f; 
+    [SerializeField] private Transform levelPartstart;
+    [SerializeField] private Transform levelPartEnding;
+    [SerializeField] private Transform levelPartOne;
+    [SerializeField] private List<Transform> levelPartList;
+    [SerializeField] private GameObject player;
+    [SerializeField] private PlayerMovementFinal playerMovement;
+    private Transform chosenLevelPart;
+    private Transform lastLevelPartTransform;
+    private Vector3 lastEndPosition;
+    int loopCounter;
+    private void Awake()
+    {
+        loopCounter = 0;
+        Transform lastLevelPartTransform;
+        lastEndPosition = levelPartstart.Find("EndPosition").position;
+        SpawnLevelPart();
+        while (loopCounter < 7)
+        {
+            SpawnLevelPart();
+            loopCounter++;
+
+        }
+        if (loopCounter >= 7)
+        {
+            SpawnEnding();
+        }
+    }
+
+    private void Update()
+    {
+    }
+
+
+    private void SpawnLevelPart()
+    {
+        chosenLevelPart = levelPartList[Random.Range(0, levelPartList.Count)];
+        lastLevelPartTransform = SpawnLevelPart(chosenLevelPart, lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform SpawnLevelPart(Transform levelPart, Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
+    }
+
+    public void SpawnEnding()
+    {
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+        Instantiate(levelPartEnding, lastEndPosition, Quaternion.identity);
+    }
+}
